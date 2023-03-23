f3dfbed22377a30ce8e5af75231aa7663953195d
Niklas <niklas.linz@enigmar.de>
2023-03-23 19:39:22 +0100

    Update

---
+++ src/main/java/de/linzn/gptFramework/completions/AIImageCompletion.java
@@ -0,0 +1,47 @@
+package de.linzn.gptFramework.completions;
+
+import com.theokanning.openai.image.CreateImageRequest;
+import com.theokanning.openai.image.Image;
+import com.theokanning.openai.service.OpenAiService;
+import de.linzn.gptFramework.GPTManager;
+import de.stem.stemSystem.STEMSystemApp;
+
+import java.time.Duration;
+import java.util.List;
+
+public class AIImageCompletion {
+    private GPTManager gptManager;
+    private OpenAiService openAiService;
+
+    public AIImageCompletion(GPTManager gptManager) {
+        this.gptManager = gptManager;
+        this.openAiService = new OpenAiService(this.gptManager.getOpenAIToken(), Duration.ofMinutes(2));
+    }
+
+    public String requestCompletion(String prompt) {
+        CreateImageRequest imageRequest = this.buildRequest(prompt);
+        String url;
+        try {
+            List<Image> results = this.openAiService.createImage(imageRequest).getData();
+            url = results.get(0).getUrl();
+        } catch (Exception e) {
+            STEMSystemApp.LOGGER.ERROR(e);
+            url = "An error was catch in kernel stacktrace! Please check STEM logs for more informations!";
+        }
+
+        return url;
+    }
+
+    private CreateImageRequest buildRequest(String prompt) {
+        return CreateImageRequest.builder()
+                .prompt(prompt)
+                .n(1)
+                .user("STEM-SYSTEM")
+                .build();
+    }
+
+    public void destroy() {
+        this.openAiService = null;
+        this.gptManager = null;
+    }
+}
