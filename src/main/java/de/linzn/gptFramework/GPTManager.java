b0bfef84b53986c27256660bc0d47f3974cc585c
Niklas <niklas.linz@enigmar.de>
2023-03-25 18:10:03 +0100

    Update

--- src/main/java/de/linzn/gptFramework/GPTManager.java
+++ src/main/java/de/linzn/gptFramework/GPTManager.java
@@ -1,40 +1,65 @@
 package de.linzn.gptFramework;
 
 import de.linzn.gptFramework.completions.AIChatCompletion;
+import de.linzn.gptFramework.completions.AIImageCompletion;
 import de.stem.stemSystem.modules.pluginModule.STEMPlugin;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GPTManager {
 
     private final String openAIToken;
-
-    private final Map<String, AIChatCompletion> aiChatCompletionMap;
+    private final Map<STEMPlugin, Map<String, AIChatCompletion>> aiChatMap;
 
     public GPTManager(STEMPlugin stemPlugin) {
         this.openAIToken = stemPlugin.getDefaultConfig().getString("openAI.token");
-        this.aiChatCompletionMap = new HashMap<>();
+        this.aiChatMap = new ConcurrentHashMap<>();
     }
 
-    public synchronized AIChatCompletion getAIChatCompletion(String identity) {
-        if (!this.aiChatCompletionMap.containsKey(identity)) {
-            AIChatCompletion aiChatCompletion = new AIChatCompletion(this);
-            this.aiChatCompletionMap.put(identity, aiChatCompletion);
+    public synchronized boolean hasAIChatCompletion(STEMPlugin stemPlugin, String identity) {
+        if (this.aiChatMap.containsKey(stemPlugin)) {
+            return this.aiChatMap.get(stemPlugin).containsKey(identity);
         }
-        return this.aiChatCompletionMap.get(identity);
+        return false;
     }
 
-    public synchronized boolean hasAIChatCompletion(String identity){
-        return this.aiChatCompletionMap.containsKey(identity);
-    }
-
-    public synchronized void destroyAIChatCompletion(String identity){
-        if(this.aiChatCompletionMap.containsKey(identity)){
-            AIChatCompletion aiChatCompletion = this.aiChatCompletionMap.get(identity);
-            aiChatCompletion.destroy();
-            this.aiChatCompletionMap.remove(identity);
+    public synchronized Map<String, AIChatCompletion> getAllAIChatCompletionByPlugin(STEMPlugin stemPlugin) {
+        if (this.aiChatMap.containsKey(stemPlugin)) {
+            return this.aiChatMap.get(stemPlugin);
+        } else {
+            return new HashMap<>();
         }
+    }
+
+    public synchronized void destroyAIChatCompletion(STEMPlugin stemPlugin, String identity) {
+        if (!this.aiChatMap.containsKey(stemPlugin)) {
+            return;
+        }
+        if (!this.aiChatMap.get(stemPlugin).containsKey(identity)) {
+            return;
+        }
+        this.aiChatMap.get(stemPlugin).get(identity).destroy();
+        this.aiChatMap.get(stemPlugin).remove(identity);
+        if (this.aiChatMap.get(stemPlugin).size() == 0) {
+            this.aiChatMap.remove(stemPlugin);
+        }
+    }
+
+    public synchronized AIChatCompletion createAIChatCompletion(STEMPlugin stemPlugin, String identity) {
+        if (!this.aiChatMap.containsKey(stemPlugin)) {
+            this.aiChatMap.put(stemPlugin, new ConcurrentHashMap<>());
+        }
+        Map<String, AIChatCompletion> pluginMap = this.aiChatMap.get(stemPlugin);
+        if (!pluginMap.containsKey(identity)) {
+            pluginMap.put(identity, new AIChatCompletion(this));
+        }
+        return pluginMap.get(identity);
+    }
+
+    public synchronized AIImageCompletion createAIImageCompletion() {
+        return new AIImageCompletion(this);
     }
 
     public String getOpenAIToken() {
